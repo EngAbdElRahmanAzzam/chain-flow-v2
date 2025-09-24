bac9d7646a9fba491ee102afbb1248d3eb67256c
Eng_AbdElRahmanAzzam <122513601+EngAbdElRahmanAzzam@users.noreply.github.com>
2025-09-24 20:06:47 +0300

    add util service  localStorage auth

--- src/ProtectedRoute/ProtectedRoute.jsx
+++ src/ProtectedRoute/ProtectedRoute.jsx
@@ -5,8 +5,8 @@ import { Navigate } from 'react-router-dom';
 
 export function ProtectedRoute({ children }) {
   const token = useSelector((state) => state.auth.token);
-  //if (!token) {
-    //return <Navigate to="/auth/login" replace />;
-  //}
+  if (!token) {
+    return <Navigate to="/auth/login" replace />;
+  }
   return children;
 }
---
+++ src/Shared/Utils/localStorgae.js
@@ -0,0 +1,22 @@
+class LocalStorageAuth{
+    static credentialsKey = 'user'
+
+    static store(data) {
+        let credentials = JSON.stringify(data)
+        localStorage.setItem(credentialsKey, credentials)
+    }
+
+    static get(data) {
+        const credentials =JSON.parse(localStorage.getItem(credentialsKey))
+        return credentials
+    }
+
+    static getToken(data) {
+        const {token} = JSON.parse(localStorage.getItem(credentialsKey));
+        return token
+    }
+
+    static clear(data) {
+        localStorage.removeItem(credentialsKey)
+    }
+}
